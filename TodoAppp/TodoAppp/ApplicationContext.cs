07f9289ef9f47f99a338e341140cd7465fca17d2
Viktor <vitalineage22@gmail.com>
2022-06-03 13:50:49 +0300

    feat: some functionality added
    Task class added. Db connection added.

---
+++ TodoAppp/TodoAppp/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAppp
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
