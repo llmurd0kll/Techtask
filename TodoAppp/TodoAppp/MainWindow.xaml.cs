dff8b2c91b18a119f51fb2ddf9c71f59e869757e
Viktor <vitalineage22@gmail.com>
2022-06-05 23:23:18 +0300

    feat: some new func added
    Sorting added, button bugs fixed, some new design features added

--- TodoAppp/TodoAppp/MainWindow.xaml.cs
+++ TodoAppp/TodoAppp/MainWindow.xaml.cs
@@ -15,23 +15,33 @@ using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Data.Entity;
-
+using System.Collections.ObjectModel;
 
 namespace TodoAppp
 {
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool flag;
+        public ObservableCollection<Task> Tasks
+        {
+            get;
+            private set;
+        }
+
 
         ApplicationContext db;
         public MainWindow()
         {
             InitializeComponent();
+            
+            Tasks = new ObservableCollection<Task>();
 
             db = new ApplicationContext();
-            db.Tasks.Load();
+            db.Tasks.Load();            
             this.DataContext = db.Tasks.Local.ToBindingList(); //Выражение db.Task.Load() загружает данные из таблицы Task в локальный кэш контекста данных. И затем список загруженных объектов устанавливается в качестве контекста данных:
         }
         // добавление
@@ -43,26 +53,31 @@ namespace TodoAppp
                 Task task = addTask.Task;
                 db.Tasks.Add(task);
                 db.SaveChanges();
+                
+                
             }
         }
+
         // редактирование
         private void Edit_Click(object sender, RoutedEventArgs e)
         {
-            // если ни одного объекта не выделено, выходим
-            if (tasksList.SelectedItem == null) return;
-            // получаем выделенный объект
-            Task task = tasksList.SelectedItem as Task;
-
-            TaskWindow taskWindow = new TaskWindow(new Task
+            // Нажатая Button         
+            Button button = sender as Button;
+            // DataContext кнопки Button будет сущностью Task
+            Task task = button.DataContext as Task;
+       
+            if (task != null)
             {
-                Id = task.Id,
-                Description = task.Description,
-                DeadlineDate = task.DeadlineDate,
-                Title = task.Title
-            });
-
+                TaskWindow taskWindow = new TaskWindow(new Task
+                {
+                    Id = task.Id,
+                    Description = task.Description,
+                    DeadlineDate = task.DeadlineDate,
+                    Title = task.Title
+                });
+            
             if (taskWindow.ShowDialog() == true)
-            {
+             {
                 // получаем измененный объект
                 task = db.Tasks.Find(taskWindow.Task.Id);
                 if (task != null)
@@ -73,17 +88,51 @@ namespace TodoAppp
                     db.Entry(task).State = EntityState.Modified; //После получения измененного объекта мы находим его в базе данных и устанавливаем у него состояние Modified, после чего сохраняем все изменения
                     db.SaveChanges();
                 }
+             }
             }
         }
         // удаление
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            // если ни одного объекта не выделено, выходим
-            if (tasksList.SelectedItem == null) return;
-            // получаем выделенный объект
-            Task task = tasksList.SelectedItem as Task;
-            db.Tasks.Remove(task);
+            // Нажатая кнопка
+            Button button = sender as Button;
+            //  DataContext выбранной кнопки будет сущностью Task
+            Task selectedItem = button.DataContext as Task;
+            if (selectedItem != null)
+            {
+                // Удаление элемента Tasks
+                db.Tasks.Remove(selectedItem);
+            }
+            //Сохранение изменений
             db.SaveChanges();
+        }
+        //сортировка по имени
+        private void SortByName_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag == false)
+            {
+                tasksList.Items.SortDescriptions.Add(new SortDescription("Title", ListSortDirection.Ascending));
+
+                flag = true;
+            }
+            else
+            {
+                MessageBox.Show("Список уже отсортирован");
+            }
+
+        }
+        //сортировка по дате
+        private void SortByDate_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag == false)
+            {
+                tasksList.Items.SortDescriptions.Add(new SortDescription("DeadlineDate", ListSortDirection.Ascending));
+                flag = true;
+            }
+            else
+            {
+                MessageBox.Show("Список уже отсортирован");
+            }
         }
     }
 }
